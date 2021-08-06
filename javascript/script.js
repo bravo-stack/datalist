f0b0f4719a04846b367020d41aadecb7923f3c45
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-08-06 23:21:38 +0100

    undid the progress bar and all it's functionalities

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,1 @@
-var i = 0;
-var progressBar = document.getElementById("pb");
-function startProgress(){
-    if(i < 100){
-        i = i + 1;
-        progressBar.value = i;
-        progressBar.textContent = i
-        progressBar.getElementsByTagName("span")[0].textContent = i;
-        console.log(i);
-    }
-    setTimeout("startProgress()", 500);
-}
-startProgress();
+console.log("welcome user")
