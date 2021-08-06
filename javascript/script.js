4c7687c988004192489ebacfc73a691d0b39bf2d
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-08-06 06:17:06 +0100

    fixed a typo in my javascript file

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 var i = 0;
 var progressBar = document.getElementById("pb");
 function startProgress(){
-    if(i <= 100){
+    if(i < 100){
         i = i + 1;
         progressBar.value = i;
         progressBar.getElementsByTagName("span")[0].textContent = i;
         console.log(i);
     }
-    setTimeout(startProgress, 500);
+    setTimeout("startProgress()", 500);
 }
 startProgress();
